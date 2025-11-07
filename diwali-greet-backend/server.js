43d21b3da4f5db772d798c334f5d2fd7c3c7323c
Rahul Tiwari <rt699652@gmail.com>
2025-11-07 21:10:08 +0530

    Fix production CORS error

--- diwali-greet-backend/server.js
+++ diwali-greet-backend/server.js
@@ -2,21 +2,21 @@ var express = require("express");
 var app = express();
 var cors = require("cors");
 var dotenv = require("dotenv");
+dotenv.config();
+
 const { dbConfig } = require("./configuration/db.config");
 const { userRouter } = require("./routers/user.router");
-const bodyParser = require("body-parser");
 const { geminiRouter } = require("./routers/gemini.router");
 var cookieParser = require("cookie-parser");
 
-dotenv.config();
+const corsOptions = {
+  origin: "https://diwali-card-acciojob.vercel.app",
+  credentials: true,
+};
 
-// middlwares
-app.use(
-  cors({
-    origin: "https://diwali-card-acciojob.vercel.app/",
-    credentials: true, // Allow cookies
-  })
-);
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/gemini", geminiRouter);
 
-var PORT = process.env.PORT || 4000;
+var PORT = process.env.PORT || 5001;
 
 app.listen(PORT, async () => {
   await dbConfig();
